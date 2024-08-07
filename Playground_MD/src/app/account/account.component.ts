3143a7413be98680b2100f109dea1bf316dbcb1f
alekhya02naidu <alekhyalsvnaidu@gmail.com>
2024-08-07 18:07:34 +0530

    added slider, chips

--- Playground_MD/src/app/account/account.component.ts
+++ Playground_MD/src/app/account/account.component.ts
@@ -11,11 +11,12 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatChipEditedEvent, MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { ProviderServiceService } from '../provider-service.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -33,8 +34,8 @@ export interface Skill {
   selector: 'app-account',
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule, MatInputModule, CommonModule, MatButtonModule,
-    MatFormFieldModule, MatRadioModule, MatDatepickerModule, MatCardModule, MatDateFnsModule, 
-    MatCheckboxModule, MatSelectModule, MatChipsModule, MatIconModule],
+    MatFormFieldModule, MatRadioModule, MatDatepickerModule, MatCardModule, MatDateFnsModule,
+    MatCheckboxModule, MatSelectModule, MatChipsModule, MatIconModule, MatSliderModule, MatAutocompleteModule],
   providers: [provideNativeDateAdapter()],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
@@ -42,6 +43,10 @@ export interface Skill {
 export class AccountComponent {
   userForm!: FormGroup;
   submitted: boolean = false;
+  currentSkill = new FormControl('');
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  readonly addOnBlur = true;
+  skills: Skill[] = [{ name: 'JavaScript' }, { name: 'Angular' }, { name: 'TypeScript' }];
 
   constructor(private formbuilder: FormBuilder, private announcer: LiveAnnouncer) {
     this.userForm = this.formbuilder.group({
@@ -51,6 +56,7 @@ export class AccountComponent {
       gender: ['', Validators.required],
       isUndergraduate: [false],
       branch: [{ value: '', disabled: true }],
+      percentage: ['',Validators.required],
       skillSet: [[]],
       hasInternship: [false]
     });
@@ -66,35 +72,35 @@ export class AccountComponent {
     });
   }
 
-  // add(event: MatChipInputEvent): void {
-  //   const value = (event.value || '').trim();
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value) {
+      this.skills = [...this.skills, { name: value }];
+    }
+    event.chipInput!.clear();
+  }
 
-  //   if (value) {
-  //     this.userForm.get('skillSet')?.value.push(value);
-  //     this.userForm.get('skillSet')?.updateValueAndValidity();
-  //   }
-  //   event.chipInput!.clear();
-  // }
+  remove(skill: Skill): void {
+    this.skills = this.skills.filter(s => s !== skill);
+    this.announcer.announce(`Removed ${skill.name}`);
+  }
 
-  // remove(skill: string): void {
-  //   const index = this.userForm.get('skillSet')?.value.indexOf(skill);
-  //   if (index >= 0) {
-  //     this.userForm.get('skillSet')?.value.splice(index, 1);
-  //   }
-  // }
+  edit(skill: Skill, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+    if (!value) {
+      this.remove(skill);
+      return;
+    }
+    this.skills = this.skills.map(s =>
+      s === skill ? { ...s, name: value } : s
+    );
+  }
 
-  // edit(skill: string, event: MatChipEditedEvent) {
-  //   const value = event.value.trim();
-  //   if (!value) {
-  //     this.remove(skill);
-  //     return;
-  //   }
-  //   this.userForm.get('skillSet')?.value = this.skills.map(s =>
-  //     s === skill ? { ...s, name: value } : s
-  //   );
-  // }
+  trackBySkill(index: number, skill: Skill): string {
+    return skill.name;
+  }
 
-  // trackBySkill(index: number, skill: Skill): string {
-  //   return skill.name;
-  // }
+  onSubmit(): void {
+    this.submitted = true;
+  }
 }
